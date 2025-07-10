05430ddbadab82acfb261acdf697cbbf1885aadb
minseok.kim <kmus1232@gmail.com>
2025-07-10 23:54:18 +0900

    EKS 권한/인증 문제 해결 중간 저장 및 최신 eks 모듈 문법 적용

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -17,3 +17,33 @@ module "vpc" {
     "Environment"                              = "prod"
   }
 }
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.36.0"
+
+  cluster_name    = "gsn-carrot-cluster"
+  cluster_version = "1.29"
+
+  subnet_ids  = module.vpc.private_subnets
+  vpc_id      = module.vpc.vpc_id
+  enable_irsa = true
+
+  cluster_endpoint_public_access       = true # 퍼블릭 엔드포인트 허용
+  cluster_endpoint_private_access      = true # 프라이빗 엔드포인트도 허용
+  cluster_endpoint_public_access_cidrs = ["119.196.71.73/32"]
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+    }
+  }
+
+  tags = {
+    Environment = "prod"
+    Terraform   = "true"
+  }
+}
--- terraform/env/prod/outputs.tf
+++ terraform/env/prod/outputs.tf
@@ -62,3 +62,29 @@ output "name" {
   description = "VPC 이름"
   value       = module.vpc.name
 }
+
+# EKS 관련 출력값들
+output "eks_cluster_name" {
+  description = "EKS 클러스터 이름"
+  value       = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS 클러스터 API 서버 엔드포인트"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_version" {
+  description = "EKS 클러스터 버전"
+  value       = module.eks.cluster_version
+}
+
+output "eks_managed_node_groups" {
+  description = "EKS 관리형 노드 그룹"
+  value       = module.eks.eks_managed_node_groups
+}
+
+output "oidc_provider_arn" {
+  description = "EKS 클러스터에 연결된 OIDC Provider의 ARN"
+  value       = module.eks.oidc_provider_arn
+}
